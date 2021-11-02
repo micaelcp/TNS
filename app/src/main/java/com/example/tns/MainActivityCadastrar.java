9117ed667fdd5f9cfcdcd6b142ea309496966406
micaelcp <69986170+micaelcp@users.noreply.github.com>
2021-11-02 19:36:44 -0300

    Atualizando classe cadastrar usuário

--- app/src/main/java/com/example/tns/MainActivityCadastrar.java
+++ app/src/main/java/com/example/tns/MainActivityCadastrar.java
@@ -1,9 +1,12 @@
 package com.example.tns;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,14 +30,53 @@ public class MainActivityCadastrar extends AppCompatActivity {
         btnCadastrarNovoU.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivityCadastrar.this, MainActivity.class);
                 Usuario usuario = new Usuario();
                 usuario.setNomeUsuario(edtNomeNovoU.getText().toString());
                 usuario.setEmialUsuario(edtEmailNovoU.getText().toString());
                 usuario.setSenha(edtSenhaNovoU.getInputType());
                 usuario.setConfirmarSenha(edtConfirmarSenha.getInputType());
-                startActivity(intent);
+                validaCampos();
             }
         });
+    }
+    private void validaCampos(){
+
+        boolean res = false;
+
+        String nome = edtNomeNovoU.getText().toString();
+        String email = edtEmailNovoU.getText().toString();
+        String senha = edtSenhaNovoU.getText().toString();
+        String confirmarSenha = edtConfirmarSenha.getText().toString();
+
+        if (res = isCampoVazio(nome)){
+            edtNomeNovoU.requestFocus();
+        }
+        else if (res = isCampoVazio(email)){
+            edtEmailNovoU.requestFocus();
+        }
+        else if (res = isCampoVazio(senha)){
+            edtSenhaNovoU.requestFocus();
+        }
+        else if (res = isCampoVazio(confirmarSenha)){
+            edtConfirmarSenha.requestFocus();
+        }
+
+        if (res){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("Aviso");
+            dlg.setMessage("Há campos inválidos ou em branco");
+            dlg.setNeutralButton("Ok", null);
+            dlg.show();
+        }
+    }
+
+    private boolean isCampoVazio(String valor){
+        boolean resultado = (TextUtils.isEmpty(valor) || valor.trim().isEmpty());
+        return resultado;
+    }
+
+    private boolean isEmailValido(String email){
+        boolean resultado = (!isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        return resultado;
     }
 }
